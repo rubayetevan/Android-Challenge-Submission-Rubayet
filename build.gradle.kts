3617869c7ef0d45d3eb36b359ad572b95e097304
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 01:51:25 +0900

    Upgrade Hilt This commit updates the following: - Upgrades the Dagger Hilt Android plugin from version 2.44.2 to 2.52.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44.2" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.protobuf") version "0.9.4" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
     kotlin("jvm") version "2.1.0"
