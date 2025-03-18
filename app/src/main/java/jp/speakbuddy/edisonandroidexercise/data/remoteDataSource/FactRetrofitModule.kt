6a6055f295867d0d1a9a9babe25ec57899c877e9
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 01:53:31 +0900

    Add FactRetrofitModule for Hilt
    This commit introduces the `FactRetrofitModule`, a Hilt module responsible for providing the `FactRetrofitService`. This module is installed in the `SingletonComponent` and utilizes the `@Provides` annotation to create and provide an instance of `FactRetrofitService` via Retrofit.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/remoteDataSource/FactRetrofitModule.kt
@@ -0,0 +1,19 @@
+package jp.speakbuddy.edisonandroidexercise.data.remoteDataSource
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FactRetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): FactRetrofitService {
+        return retrofit.create(FactRetrofitService::class.java)
+    }
+}
