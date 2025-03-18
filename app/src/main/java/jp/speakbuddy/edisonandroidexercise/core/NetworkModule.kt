9931a024e7bb00c73a587d21cb75633f91f02ea7
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 01:54:05 +0900

    Add Network Module for Retrofit and Kotlin Serialization
    This commit introduces a `NetworkModule` to manage network-related dependencies using Hilt. It includes the following:
    
    -   Provides an `OkHttpClient` instance for network requests.
    -   Provides a `Retrofit` instance configured with:
        -   The base URL from `BuildConfig.BASE_URL`.
        -   The provided `OkHttpClient`.
        -   A converter factory using `kotlinx.serialization.Json` to handle JSON serialization and deserialization for Retrofit responses.
        -   Use `application/json` as media type for JSON serialization.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/core/NetworkModule.kt
@@ -0,0 +1,32 @@
+package jp.speakbuddy.edisonandroidexercise.core
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.speakbuddy.edisonandroidexercise.BuildConfig
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType())).build()
+    }
+}
