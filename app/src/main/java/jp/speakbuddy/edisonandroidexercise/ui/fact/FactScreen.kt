45392785d64ab199f36f65147afd0683af6eb393
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:30:04 +0900

    Refactor: Use `collectAsStateWithLifecycle` in FactScreen and update UI
    This commit refactors `FactScreen` to use `collectAsStateWithLifecycle` for state management and updates the UI based on the new state:
    - Replaces `mutableStateOf` and `remember` with `collectAsStateWithLifecycle` for managing the UI state from `FactViewModel`.
    - Removes temporary result.
    - Displays the fact from the `uiState`.
    - Displays the fact length when `showFactLength` is true.
    - Displays a "Multiple cats!" message when `isMultipleCats` is true.
    - Disables the "Update fact" button while loading (`isLoading`).
    - Add `Spacer`.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactScreen.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactScreen.kt
@@ -3,20 +3,23 @@ package jp.speakbuddy.edisonandroidexercise.ui.fact
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.speakbuddy.edisonandroidexercise.ui.theme.EdisonAndroidExerciseTheme
 
@@ -24,6 +27,8 @@ import jp.speakbuddy.edisonandroidexercise.ui.theme.EdisonAndroidExerciseTheme
 fun FactScreen(
     viewModel: FactViewModel = viewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -35,25 +40,40 @@ fun FactScreen(
             alignment = Alignment.CenterVertically
         )
     ) {
-        var fact by remember { mutableStateOf("") }
-
         Text(
             text = "Fact",
             style = MaterialTheme.typography.titleLarge
         )
 
-        Text(
-            text = fact,
-            style = MaterialTheme.typography.bodyLarge
-        )
-
-        val onClick = {
-            fact = viewModel.updateFact { print("done") }
+        uiState.fact?.let { fact ->
+            Text(
+                text = fact,
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
 
-        Button(onClick = onClick) {
+        if(uiState.showFactLength){
+            Text(
+                text = "${uiState.factLength}",
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp
+                )
+            )
+        }
+
+        Button(onClick = { viewModel.updateFact() }, enabled = !uiState.isLoading) {
             Text(text = "Update fact")
         }
+        Spacer(Modifier.height(30.dp))
+        if (uiState.isMultipleCats)
+            Text(
+                text = "Multiple cats!",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            )
     }
 }
 
