68ade1f024fb50c6ecc1ad0d1b1b005ccfc23d92
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 20:10:44 +0900

    Feat: Create DataStoreModule for FactProto
    This commit introduces `DataStoreModule`, a Hilt module that provides a `DataStore` instance for `FactProto`. The `provideDataStore` function creates and configures the `DataStore` to store `FactProto` data in a file named "fact.pb".
    - Adds `DataStoreModule` to manage DataStore instance creation and configuration.
    - Utilizes `DataStoreFactory` to create a DataStore instance.
    - Sets up file creation, corruption handling, and coroutine scope for `DataStore`.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/dataSources/localDataSource/DataStoreModule.kt
@@ -0,0 +1,36 @@
+package jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.localDataSource
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import jp.speakbuddy.edisonandroidexercise.FactProto
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Singleton
+
+private const val FACT_PROTOBUF_FILE_NAME = "fact.pb"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(
+        @ApplicationContext context: Context,
+        coroutineScope: CoroutineScope
+    ): DataStore<FactProto> {
+        return DataStoreFactory.create(
+            serializer = FactSerializer,
+            produceFile = { context.filesDir.resolve(FACT_PROTOBUF_FILE_NAME) },
+            corruptionHandler = ReplaceFileCorruptionHandler {
+                FactProto.getDefaultInstance()
+            },
+            scope = coroutineScope
+        )
+    }
+}
