635f91bff8ae972e55e566f6aad9220e2461edd9
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:29:37 +0900

    Refactor: Update FactService and FactServiceImpl
    This commit refactors `FactService` and `FactServiceImpl` with the following changes:
    - Updates `FactService` to use `Flow<Result<FactResponse>>` instead of `FactResponse`.
    - Updates `FactServiceImpl` to get fact from `FactRepository` and use the new `getNewFact()` method.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/domain/fact/FactServiceImpl.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/domain/fact/FactServiceImpl.kt
@@ -1,7 +1,11 @@
 package jp.speakbuddy.edisonandroidexercise.domain.fact
 
+import jp.speakbuddy.edisonandroidexercise.core.Result
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
 import jp.speakbuddy.edisonandroidexercise.data.fact.repository.FactRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class FactServiceImpl @Inject constructor(private val factRepository: FactRepository) : FactService {
+    override fun getNewFact(): Flow<Result<FactResponse>>  = factRepository.getNewFact()
 }
