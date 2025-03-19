ba9330aa3a4873714b3d8a38b538b4ac950f7054
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:29:57 +0900

    Refactor: Implement Fact fetching logic in FactViewModel
    This commit refactors `FactViewModel` to implement the logic for fetching and updating a fact from the `FactService`. The main changes include:
    - Adds `updateFact()` to fetch a new fact and update the UI state.
    - Introduce a `uiState` `StateFlow` that emits `FactUiState` to reflect the state of fact loading and content.
    - Introduces a loading state into the FactUiState.
    - Handle `Result.Loading`, `Result.Error` and `Result.Success` from the `FactService`.
    - Removes the previous `updateFact` method, which was not correctly implemented.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactViewModel.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactViewModel.kt
@@ -2,19 +2,61 @@ package jp.speakbuddy.edisonandroidexercise.ui.fact
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.speakbuddy.edisonandroidexercise.core.Result
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
 import jp.speakbuddy.edisonandroidexercise.domain.fact.FactService
-import kotlinx.coroutines.runBlocking
+import jp.speakbuddy.edisonandroidexercise.ui.fact.models.FactUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FactViewModel @Inject constructor(private val factService: FactService) : ViewModel() {
-    fun updateFact(completion: () -> Unit): String =
-        runBlocking {
-            try {
-                " no fact available tight now"
-            } catch (e: Throwable) {
-                "something went wrong. error = ${e.message}"
-            }.also { completion() }
+
+    private val _uiState = MutableStateFlow(FactUiState())
+    val uiState: StateFlow<FactUiState> = _uiState.onStart {
+        updateFact()
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = FactUiState()
+    )
+
+    fun updateFact() {
+        viewModelScope.launch {
+            factService.getNewFact().collect { result ->
+                when (result) {
+                    is Result.Error -> {
+                        _uiState.update {
+                            it.copy(isLoading = false)
+                        }
+                    }
+
+                    is Result.Loading -> {
+                        _uiState.update {
+                            it.copy(isLoading = true)
+                        }
+                    }
+
+                    is Result.Success<FactResponse> -> {
+                        _uiState.update {
+                            it.copy(
+                                fact = result.data.fact,
+                                factLength = result.data.length,
+                                isLoading = false
+                            )
+                        }
+                    }
+                }
+
+            }
         }
+    }
 }
