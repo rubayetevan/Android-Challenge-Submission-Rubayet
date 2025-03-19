82e2ec41589076d49090b45783414c7dc374189f
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:29:20 +0900

    Refactor: Create FactRemoteDataSource and FactRemoteDataSourceImpl
    This commit introduces the following changes:
    - Creates `FactRemoteDataSource` interface with `getNewFact()` method, which returns a `Flow` of `Result<FactResponse>`.
    - Creates `FactRemoteDataSourceImpl` implementing `FactRemoteDataSource`.
    - Adds implementation of `getNewFact()` in `FactRemoteDataSourceImpl`, using `transformToResult` to handle the response from `FactRetrofitService`.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/dataSources/remoteDataSource/FactRemoteDataSource.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/dataSources/remoteDataSource/FactRemoteDataSource.kt
@@ -1,4 +1,9 @@
 package jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource
 
+import jp.speakbuddy.edisonandroidexercise.core.Result
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
+import kotlinx.coroutines.flow.Flow
+
 interface FactRemoteDataSource {
+    fun getNewFact(): Flow<Result<FactResponse>>
 }
--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/dataSources/remoteDataSource/FactRemoteDataSourceImpl.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/dataSources/remoteDataSource/FactRemoteDataSourceImpl.kt
@@ -1,11 +1,20 @@
 package jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource
 
+import jp.speakbuddy.edisonandroidexercise.core.Result
+import jp.speakbuddy.edisonandroidexercise.core.transformToResult
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
 import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.network.FactRetrofitService
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class FactRemoteDataSourceImpl @Inject constructor(
     private val factRetrofitService: FactRetrofitService,
     private val coroutineScope: CoroutineScope
 ) : FactRemoteDataSource {
+    override fun getNewFact(): Flow<Result<FactResponse>> {
+       return transformToResult(coroutineScope.coroutineContext) {
+           factRetrofitService.getFact()
+       }
+    }
 }
