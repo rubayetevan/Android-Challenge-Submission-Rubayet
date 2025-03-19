c6dc295f66390536fb37c4412fbac54acf5ac881
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:29:29 +0900

    Refactor: Update FactRepository to use Flow and Result
    This commit updates the `FactRepository` and `FactRepositoryImpl` to use `Flow` and `Result` for handling asynchronous operations and results.
    - Adds a `getNewFact()` method to `FactRepository` that returns a `Flow` of `Result<FactResponse>`.
    - Implements `getNewFact()` in `FactRepositoryImpl` to delegate to `factRemoteDataSource.getNewFact()`.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/repository/FactRepository.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/repository/FactRepository.kt
@@ -1,4 +1,9 @@
 package jp.speakbuddy.edisonandroidexercise.data.fact.repository
 
+import jp.speakbuddy.edisonandroidexercise.core.Result
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
+import kotlinx.coroutines.flow.Flow
+
 interface FactRepository {
+    fun getNewFact(): Flow<Result<FactResponse>>
 }
--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/repository/FactRepositoryImpl.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/data/fact/repository/FactRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package jp.speakbuddy.edisonandroidexercise.data.fact.repository
 
+import jp.speakbuddy.edisonandroidexercise.core.Result
 import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.FactRemoteDataSource
+import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class FactRepositoryImpl @Inject constructor(private val factRemoteDataSource: FactRemoteDataSource) :
-    FactRepository {
+class FactRepositoryImpl @Inject constructor(private val factRemoteDataSource: FactRemoteDataSource) : FactRepository {
+    override fun getNewFact(): Flow<Result<FactResponse>> = factRemoteDataSource.getNewFact()
 }
