113fa7012218443150b8c5e338d9de21b1bcaa58
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:29:46 +0900

    feat: Introduce `FactUiState`
    This commit introduces `FactUiState` to encapsulate the UI state for displaying a fact. It includes:
    - `fact`: The fact string.
    - `factLength`: The length of the fact string.
    - `isLoading`: A flag indicating if the fact is being loaded.
    - `showFactLength`: A computed property indicating if the fact length should be displayed (when length > 100).
    - `isMultipleCats`: A computed property indicating if the fact contains the word "cats" multiple times.
    - `hasCats`: A private function to check if the word "cats" is present in the fact string.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/models/FactUiState.kt
@@ -0,0 +1,17 @@
+package jp.speakbuddy.edisonandroidexercise.ui.fact.models
+
+data class FactUiState(
+    val fact: String? = null,
+    val factLength: Int = 0,
+    val isLoading: Boolean = false
+) {
+
+    val showFactLength: Boolean = factLength > 100
+    val isMultipleCats: Boolean = hasCats(fact)
+
+    private fun hasCats(fact: String?): Boolean {
+        fact ?: return false
+        val catsCount = fact.lowercase().split(" ").count { it == "cats" }
+        return catsCount > 0
+    }
+}
