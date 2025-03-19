f864b247126cb47bdd16c53e290620a63dda6853
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 16:16:52 +0900

    feat: Implement transformToResult for network requests
    This commit introduces the `transformToResult` function, which provides a standardized way to handle network requests and their results within a Kotlin Flow. This function encapsulates the logic for:
    
    -   Emitting a `Result.Loading` state at the beginning of the request.
    -   Executing the network call within a provided `CoroutineContext`.
    -   Handling successful responses by emitting a `Result.Success` containing the response body.
    -   Handling null response bodies by emitting a `Result.Error`.
    -   Handling unsuccessful HTTP responses by extracting the error code and message from the response and emitting a `Result.Error`.
    -   Catching and handling common network-related exceptions such as `ConnectException`, `SocketTimeoutException`, and `IOException`, emitting appropriate `Result.Error` values.
    -   Catching unexpected exceptions and emitting a generic `Result.Error`.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/core/TransformToResult.kt
@@ -0,0 +1,55 @@
+package jp.speakbuddy.edisonandroidexercise.core
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import kotlin.coroutines.CoroutineContext
+
+
+fun <T> transformToResult(
+    context: CoroutineContext,
+    block: suspend () -> Response<T>
+): Flow<Result<T>> = flow {
+    emit(Result.Loading)
+
+    try {
+        val response = withContext(context) {
+            block()
+        }
+        if (response.isSuccessful) {
+            val body = response.body()
+            if (body != null) {
+                emit(Result.Success(body))
+            } else {
+                emit(Result.Error(ResultError(response.code(), "Response body is null")))
+            }
+        } else {
+            val errorCode = response.code()
+            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+            emit(Result.Error(ResultError(errorCode, errorMessage)))
+        }
+    } catch (exception: Exception) {
+        when (exception) {
+            is ConnectException -> {
+                emit(Result.Error(ResultError(-1, "No Internet Error: ${exception.message}")))
+            }
+
+            is SocketTimeoutException -> {
+                emit(Result.Error(ResultError(-2, "Socket Timeout error: ${exception.message}")))
+            }
+
+            is IOException -> {
+                emit(Result.Error(ResultError(-3, "Network error: ${exception.message}")))
+            }
+
+            else -> {
+                emit(Result.Error(ResultError(-4, "Unexpected error: ${exception.message}")))
+            }
+        }
+    }
+
+}
