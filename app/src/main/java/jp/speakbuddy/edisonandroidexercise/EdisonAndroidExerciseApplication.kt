27cbf2d9238c0033adf02970c90f0294eaaa9ce5
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-19 22:09:54 +0900

    Feat: Add Coil image loader configuration
    This commit adds configuration for the Coil image loader, including:
    - Setting up a memory cache with a maximum size of 25% of the available memory.
    - Setting up a disk cache with a maximum size of 2% of the available disk space, storing images in the `IMAGE_CACHE_DIRECTORY`.
    - Enabling crossfade transition for smoother image loading.
    - Implementing `SingletonImageLoader.Factory` to use this custom configuration.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/EdisonAndroidExerciseApplication.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/EdisonAndroidExerciseApplication.kt
@@ -1,7 +1,32 @@
 package jp.speakbuddy.edisonandroidexercise
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.crossfade
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EdisonAndroidExerciseApplication : Application()
+class EdisonAndroidExerciseApplication : Application(), SingletonImageLoader.Factory {
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .crossfade(true)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve(BuildConfig.IMAGE_CACHE_DIRECTORY))
+                    .maxSizePercent(0.02)
+                    .build()
+            }.build()
+    }
+
+}
