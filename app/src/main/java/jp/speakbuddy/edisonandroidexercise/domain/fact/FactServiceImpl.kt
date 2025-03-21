aaeed34c54ce7ec578d24aec647ed91aead5c9e3
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-21 14:12:20 +0900

    Refactor: Simplify FactServiceImpl constructor and `getNewFact`
    This commit refactors `FactServiceImpl` by simplifying its constructor and the `getNewFact` function.
    - Removes the saving of the new fact within `getNewFact` function, which was previously done inside `onEach`.
    - Removes the `first()` function in the `getLastFact` function.
    - Update `FactServiceImpl` constructor, it's a simple constructor with parameter of `FactRepository` now.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/domain/fact/FactServiceImpl.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/domain/fact/FactServiceImpl.kt
@@ -4,17 +4,11 @@ import jp.speakbuddy.edisonandroidexercise.core.Result
 import jp.speakbuddy.edisonandroidexercise.data.fact.dataSources.remoteDataSource.models.FactResponse
 import jp.speakbuddy.edisonandroidexercise.data.fact.repository.FactRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class FactServiceImpl @Inject constructor(private val factRepository: FactRepository) :
     FactService {
-    override fun getNewFact(): Flow<Result<FactResponse>> = factRepository.getNewFact().onEach {
-        if (it is Result.Success) {
-            factRepository.saveFact(it.data)
-        }
-    }
+    override fun getNewFact(): Flow<Result<FactResponse>> = factRepository.getNewFact()
 
-    override suspend fun getLastFact(): FactResponse? = factRepository.getLastFact().first()
+    override suspend fun getLastFact(): FactResponse? = factRepository.getLastFact()
 }
