8dad43b47280ec0a13950d7fec002cf3a3314f08
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-22 00:52:33 +0900

    Feat: Configure Coil image loader with OkHttp
    This commit configures the Coil image loader to use OkHttp for network requests.
    - Adds `OkHttpNetworkFetcherFactory` to the `ImageLoader`'s components.
    - Injects `OkHttpClient` to create the `OkHttpNetworkFetcherFactory`.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/EdisonAndroidExerciseApplication.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/EdisonAndroidExerciseApplication.kt
@@ -7,15 +7,30 @@ import coil3.SingletonImageLoader
 import coil3.disk.DiskCache
 import coil3.disk.directory
 import coil3.memory.MemoryCache
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import coil3.request.crossfade
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.OkHttpClient
+import javax.inject.Inject
 
 @HiltAndroidApp
 class EdisonAndroidExerciseApplication : Application(), SingletonImageLoader.Factory {
 
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
     override fun newImageLoader(context: PlatformContext): ImageLoader {
         return ImageLoader.Builder(context)
             .crossfade(true)
+            .components {
+                add(
+                    OkHttpNetworkFetcherFactory(
+                        callFactory = {
+                            okHttpClient
+                        }
+                    )
+                )
+            }
             .memoryCache {
                 MemoryCache.Builder()
                     .maxSizePercent(context, BuildConfig.IMAGE_MEMORY_CACHE_PERCENT)
