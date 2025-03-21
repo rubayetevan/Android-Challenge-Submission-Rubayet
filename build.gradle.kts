a68cf9fb7dc25573068a94e0e08d6e23e4db3871
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-22 00:52:15 +0900

    Refactor: Update Kotlin and Compose versions to 2.0.0
    This commit updates the Kotlin and Compose versions in the `build.gradle.kts` file to 2.0.0.
    - Updates the `org.jetbrains.kotlin.android` plugin version.
    - Updates the `org.jetbrains.kotlin.plugin.compose` plugin version.
    - Updates the Kotlin JVM version.
    - Updates the `kotlin.plugin.serialization` version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.protobuf") version "0.9.4" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.serialization") version "2.1.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
