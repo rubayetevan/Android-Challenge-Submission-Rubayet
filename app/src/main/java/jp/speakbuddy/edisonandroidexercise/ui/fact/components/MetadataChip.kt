15fe8521db3d69f3719abecad3dc7554fd1494d4
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-22 17:57:31 +0900

    Feat: Implement MetadataChip composable
    This commit introduces the `MetadataChip` composable, a UI element that displays metadata with a rounded background.
    - Implements `MetadataChip` with `AnimatedVisibility` to show the metadata.
    - Uses `Box` with a rounded background for the chip.
    - Displays text using `MaterialTheme.typography.labelLarge`.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/MetadataChip.kt
@@ -0,0 +1,32 @@
+package jp.speakbuddy.edisonandroidexercise.ui.fact.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MetadataChip(visible: Boolean, text: String, backgroundColor: Color) {
+    AnimatedVisibility(visible = visible) {
+        Box(
+            modifier = Modifier
+                .background(
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(15.dp)
+                )
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp),
+                text = text,
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
+    }
+}
