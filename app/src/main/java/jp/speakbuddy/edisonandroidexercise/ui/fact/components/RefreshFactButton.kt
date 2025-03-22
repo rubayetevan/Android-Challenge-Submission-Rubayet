63e9935192c0dd3495b40989eefeea0de3b9a90a
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-22 17:57:40 +0900

    Feat: Implement RefreshFactButton composable
    This commit introduces the `RefreshFactButton` composable, enabling the user to refresh the displayed fact.
    - Creates `RefreshFactButton` which is a `Button` that shows a text to update the fact.
    - Receives `onClick` function to trigger when the button is pressed, and `enabled` to manage the button status.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/RefreshFactButton.kt
@@ -0,0 +1,17 @@
+package jp.speakbuddy.edisonandroidexercise.ui.fact.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import jp.speakbuddy.edisonandroidexercise.R
+
+@Composable
+fun RefreshFactButton(onClick: () -> Unit, enabled: Boolean) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+    ) {
+        Text(text = stringResource(R.string.update_fact))
+    }
+}
