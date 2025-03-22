e4fc2fab6e362e5023b9e40fb812609b3bd8e83c
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-22 17:57:51 +0900

    Feat: Implement portrait content layout for FactScreen
    This commit introduces a new `FactScreenContentPortrait` composable to handle the portrait layout for the `FactScreen`.
    - Adds `FactScreenContentPortrait` composable that contains `CatImage` and `FactSection`.
    - Implements vertical scrolling in `FactScreenContentPortrait`.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/FactScreenContentPortrait.kt
@@ -0,0 +1,29 @@
+package jp.speakbuddy.edisonandroidexercise.ui.fact.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import jp.speakbuddy.edisonandroidexercise.ui.fact.CAT_IMAGE_URL
+import jp.speakbuddy.edisonandroidexercise.ui.fact.models.FactUiState
+
+
+@Composable
+fun FactScreenContentPortrait(uiState: FactUiState) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 16.dp)
+    ) {
+        CatImage(CAT_IMAGE_URL)
+        FactSection(uiState)
+    }
+}
