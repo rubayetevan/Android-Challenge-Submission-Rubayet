6c502f6e91fbd4c6693636074ae5d22339723be2
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-23 11:53:05 +0900

    Feat: Add EmptyFactSection composable
    This commit introduces the `EmptyFactSection` composable, which is displayed when there are no facts available. It provides a user-friendly message and instructions.
    - Adds a new file `EmptyFactSection.kt` with the composable function.
    - Includes text indicating no facts are present.
    - Adds a call to action for the user to tap update.

---
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/EmptyFactSection.kt
@@ -0,0 +1,41 @@
+package jp.speakbuddy.edisonandroidexercise.ui.fact.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import jp.speakbuddy.edisonandroidexercise.R
+
+@Composable
+fun EmptyFactSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.curious_about_cats),
+            style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.tap_update_to_learn),
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center
+        )
+    }
+}
