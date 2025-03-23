87cc15156f13343ea8322ffd56e0b316789f8824
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-23 11:38:01 +0900

    Feat: Add Adaptive library dependency
    This commit adds the `adaptive-android` library as a dependency, to use adaptive layouts for different screen sizes.
    - Added `implementation("androidx.compose.material3.adaptive:adaptive-android:1.1.0")` to the `build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,7 +115,7 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
-
+    implementation("androidx.compose.material3.adaptive:adaptive-android:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
