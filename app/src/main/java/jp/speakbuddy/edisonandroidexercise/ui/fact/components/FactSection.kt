5a583ffcd55dd4801141ee008bd8f429268df9f0
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-23 11:38:54 +0900

    Refactor: Remove title from FactSection and add modifier to FactCard
    This commit refactors `FactSection` by removing the title text and passing a modifier to the `FactCard`.
    - Removes the `Text` component from `FactSection`, eliminating the title.
    - Adds a `modifier` parameter to `FactSection` for layout flexibility.
    - Passes the `modifier` to the `FactCard` for further customization.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/FactSection.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/FactSection.kt
@@ -1,19 +1,12 @@
 package jp.speakbuddy.edisonandroidexercise.ui.fact.components
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import jp.speakbuddy.edisonandroidexercise.R
+import androidx.compose.ui.Modifier
 import jp.speakbuddy.edisonandroidexercise.ui.fact.models.FactUiState
 
 @Composable
-fun FactSection(uiState: FactUiState) {
-    Text(
-        text = stringResource(R.string.fact),
-        style = MaterialTheme.typography.titleLarge,
-    )
+fun FactSection(uiState: FactUiState, modifier: Modifier = Modifier) {
     uiState.fact?.let { fact ->
-        FactCard(fact, uiState)
+        FactCard(fact, uiState, modifier = modifier)
     }
 }
