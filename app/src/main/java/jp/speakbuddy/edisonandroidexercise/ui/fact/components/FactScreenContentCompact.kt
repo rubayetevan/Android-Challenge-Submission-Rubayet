23505942ba04c50189c47e6fc78e34dfec2998cb
Md Rubayet Hassan <rubayet.hassan@splyza.com>
2025-03-23 11:39:45 +0900

    Refactor: Rename `FactScreenContentPortrait` to `FactScreenContentCompact` and update UI
    This commit renames `FactScreenContentPortrait` to `FactScreenContentCompact` and adjusts the UI layout.
    - Renames `FactScreenContentPortrait` to `FactScreenContentCompact`.
    - Centers the `CatImage` horizontally within the `Column`.
    - Sets `FactSection` width to fill available space.

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/FactScreenContentCompact.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/components/FactScreenContentCompact.kt
@@ -3,10 +3,12 @@ package jp.speakbuddy.edisonandroidexercise.ui.fact.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import jp.speakbuddy.edisonandroidexercise.ui.fact.CAT_IMAGE_URL
@@ -14,7 +16,7 @@ import jp.speakbuddy.edisonandroidexercise.ui.fact.models.FactUiState
 
 
 @Composable
-fun FactScreenContentPortrait(uiState: FactUiState) {
+fun FactScreenContentCompact(uiState: FactUiState) {
     val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
@@ -23,7 +25,7 @@ fun FactScreenContentPortrait(uiState: FactUiState) {
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(space = 16.dp)
     ) {
-        CatImage(CAT_IMAGE_URL)
-        FactSection(uiState)
+        CatImage(CAT_IMAGE_URL, modifier = Modifier.align(Alignment.CenterHorizontally))
+        FactSection(uiState, modifier = Modifier.fillMaxWidth())
     }
 }
